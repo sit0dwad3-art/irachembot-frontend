e60aef5029a5b57177d087f4fcf55e99651ef263
GONZALO RIFFO VILLEGAS <sit0dwad3@gmail.com>
2026-05-06 15:35:43 +0200

    fix errors

--- next.config.ts
+++ next.config.ts
@@ -4,17 +4,7 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs:  false,
-        net: false,
-        tls: false,
-      }
-    }
-    return config
-  },
+  turbopack: {},
 }
 
 export default nextConfig
